a77c0233bb6034645d4b0796edfcf7a68eed0e06
bpuuqd <bpuuqd@gmail.com>
2021-04-16 04:20:51 +0900

    style edit

--- src/App.js
+++ src/App.js
@@ -72,7 +72,7 @@ function App() {
 
       <div className="app__left">
         <div className="app__header">
-          <h1>Covid-19 tracker</h1>
+          <h1>Covid-19 Tracker</h1>
           <FormControl className="app__dropdown">
             <Select variant="outlined" onChange={onCountryChange} value={country}>
               <MenuItem value="worldwide">Worldwide</MenuItem>
